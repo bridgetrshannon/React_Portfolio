586979f2cd84a0c74a1dae355a9de578f75fad0a
Bridget Shannon <bridgetrshannon@gmail.com>
2020-11-12 22:27:28 -0500

    added bootstrap cdn to index.html file

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,17 +1,23 @@
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Project from "./components/Project";
-import Footer from "./components/Footer";
-
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Header />
-      <Project />
-      <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

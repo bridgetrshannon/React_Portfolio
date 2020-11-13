5bd778cec39d225c23aeb3fe7c61fe808d2063ce
Bridget Shannon <bridgetrshannon@gmail.com>
2020-11-12 22:58:42 -0500

    added templates to component pages, content now rendering for home page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,6 +6,7 @@ import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
             <Route path="/contact" component={Contact} />
             <Route component={Error} />
           </Switch>
+          <Footer />
         </div>
       </BrowserRouter>
     );

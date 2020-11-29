dd31abe9b500bd33e62ff0f2a0a8b39ebbe2cad5
Bridget Shannon <bridgetrshannon@gmail.com>
2020-11-29 18:33:04 -0500

    image is now fullscreen on landing page

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -2,9 +2,26 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
+    <body className="backgroundImg">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 transboxHome text-center mt-5">
+            <br />
+            <h2 className="mt-5">Bridget Shannon</h2>
+            <h2 className="mb-5">Full Stack Web Developer</h2>
+            <br />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 transboxHome text-center"></div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 transboxHome text-center">
+            <br />
+          </div>
+        </div>
+      </div>
+    </body>
   );
 };
 
